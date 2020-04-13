6d643d79fe1080cd105499f21808b383b3d0c644
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2020-04-13 03:42:09 -0400

    Simplified Simple Search - no data class

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,72 +1,46 @@
 package search
 
+private val data = mutableListOf<String>()
+
 fun main() {
-    readInput()
+    inputPeople()
     do {
-        printHeader("Menu")
-        println("1. Find a person")
-        println("2. Print all people")
-        println("0. Exit")
+        """${header("Menu")}
+        |1. Find a person
+        |2. Print all people")
+        |println("0. Exit")""".trimMargin().also(::println)
+
         val choice = readLine()!!.toInt()
         when (choice) {
             0 -> println("\nBye!")
-            1 -> runQuery()
-            2 -> listEntries()
+            1 -> query()
+            2 -> list()
             else -> println("Incorrect option! Try again.")
         }
     } while (choice != 0)
 }
 
-fun printHeader(title: String) = println("\n=== $title ===")
+private fun header(title: String) = println("\n=== $title ===")
 
-fun runQuery() {
-    println("Enter a name or email to search all suitable people.")
-    val value = readLine()!!.trim()
-    val matches = data.filter { it.matchesAny(value) }
-    if (matches.isNotEmpty()) {
-        matches.joinToString("\n")
-    } else {
-        "No matching people found."
-    }.also(::println)
-}
-
-fun listEntries() {
-    printHeader("List of people")
-    println(data.joinToString("\n"))
-}
-
-private val data = mutableListOf<Entry>()
-
-private fun readInput() {
+private fun inputPeople() {
     println("Enter the number of people:")
     val count = readLine()!!.toInt()
     println("Enter all people:")
-    repeat(count) {
-        val input = readLine()!!.split(" ").map { it.trim() }
-        when (input.size) {
-            1 -> {
-                data.add(Entry(input.first()))
-            }
-            2 -> {
-                val (first, last) = input
-                data.add(Entry(first, last))
-            }
-            3 -> {
-                val (first, last, email) = input
-                data.add(Entry(first, last, email))
-            }
-        }
+    repeat(count) { data.add(readLine()!!) }
+}
+
+private fun query() {
+    println("Enter a name or email to search all suitable people.")
+    val value = readLine()!!.trim()
+    val matches = data.filter { it.contains(value, ignoreCase = true) }
+    if (matches.isNotEmpty()) {
+        matches.forEach(::println)
+    } else {
+        println("No matching people found.")
     }
 }
 
-data class Entry(
-        val first: String,
-        val last: String = "",
-        val email: String = "") {
-
-    override fun toString(): String =
-        listOf<String>(first, last, email).joinToString(" ").trim()
-
-    fun matchesAny(value: String): Boolean =
-        toString().contains(value, ignoreCase = true)
+private fun list() {
+    println(header("List of people"))
+    data.forEach(::println)
 }
