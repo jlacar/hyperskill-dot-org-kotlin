c1982817e7c88b33b4870436483f61488d3bbb63
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2020-04-17 09:03:21 -0400

    Refactored explaining variables, extracted printHeader()

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -19,7 +19,7 @@ fun main(args: Array<String>) {
         when (choice) {
             0 -> println("\nBye!")
             1 -> query(people, index)
-            2 -> list(people)
+            2 -> list(people, "indexed")
             else -> println("Incorrect option! Try again.")
         }
     } while (choice != 0)
@@ -42,7 +42,7 @@ private fun index(people: List<String>): Map<String, List<Int>> {
 private fun query(people: List<String>, index: Map<String, List<Int>>) {
     println("Select a matching strategy: ${SearchStrategy.values().joinToString()}")
     val search = SearchStrategy.valueOf(readLine()!!.toUpperCase())
-    list(search.results(getTerms(), people, index))
+    list(search.results(getTerms(), people, index), "match $search")
 }
 
 private fun header(title: String) = "\n=== $title ==="
@@ -52,38 +52,45 @@ private fun getTerms(): List<String> {
     return readLine()!!.trim().toLowerCase().split(" ")
 }
 
-private fun list(people: List<String>) {
+private fun list(people: List<String>, description: String) {
     if (people.isNotEmpty()) {
-        println(header("List of people"))
+        printHeader(people, description)
         people.forEach(::println)
     } else {
-        println("No matching people found.")
+        println("No people found.")
     }
+}
+
+private fun printHeader(people: List<String>, description: String) {
+    val qualifier = if (people.size == 1) "person" else "people"
+    println(header("${people.size} $qualifier $description"))
 }
 
 enum class SearchStrategy {
     ALL {
-        override fun results(words: List<String>, people: List<String>, index: Map<String, List<Int>>): List<String> {
+        override fun results(words: List<String>, people: List<String>, index: Map<String, List<Int>>):
+                List<String> {
             val allWords = words.size
-            val matchCounts = allIndicesOf(words, index).groupingBy { it }.eachCount()
-            return matchCounts.filter { (_, matched) -> matched == allWords }.map { people[it.key] }
+            val matchFrequencies = allIndicesOf(words, index).groupingBy { it }.eachCount()
+            return matchFrequencies.filter { (_, itMatches) -> itMatches == allWords }.map { people[it.key] }
         }
     },
 
     ANY {
-        override fun results(words: List<String>, people: List<String>, index: Map<String, List<Int>>): List<String> =
-            allIndicesOf(words, index).distinct().map { people[it] }
+        override fun results(words: List<String>, people: List<String>, index: Map<String, List<Int>>):
+                List<String> = allIndicesOf(words, index).distinct().map { people[it] }
     },
 
     NONE {
-        override fun results(words: List<String>, people: List<String>, index: Map<String, List<Int>>): List<String> {
+        override fun results(words: List<String>, people: List<String>, index: Map<String, List<Int>>):
+                List<String> {
             val anyMatches = ANY.results(words, people, index)
             return people.filterNot { anyMatches.contains(it) }
         }
     };
 
-    protected fun allIndicesOf(words: List<String>, index: Map<String, List<Int>>): List<Int> =
-            words.mapNotNull { index[it] }.flatten()
+    protected fun allIndicesOf(words: List<String>, index: Map<String, List<Int>>):
+            List<Int> = words.mapNotNull { index[it] }.flatten()
 
     abstract fun results(words: List<String>, people: List<String>, index: Map<String, List<Int>>): List<String>
 }
