28939de2ca21913cc4453b37b89c2569cc80856f
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2020-04-20 13:18:25 -0400

    Added support for floating point values

--- Numeric_Matrix_Processor/Numeric Matrix Processor/task/src/processor/Main.kt
+++ Numeric_Matrix_Processor/Numeric Matrix Processor/task/src/processor/Main.kt
@@ -1,28 +1,28 @@
 package processor
 
-class IntMatrix(val rows: Int, val cols: Int) {
+class Matrix(val rows: Int, val cols: Int) {
     val size = Pair(rows, cols)
-    private val elements = Array<IntArray>(rows) { IntArray(cols) { 0 } }
+    private val elements = Array<DoubleArray>(rows) { DoubleArray(cols) { 0.0 } }
 
     override fun toString(): String = elements.joinToString("\n") {
         it.joinToString(" ")
     }
 
-    operator fun plus(other: IntMatrix): IntMatrix? {
+    operator fun plus(other: Matrix): Matrix? {
         if (this.size != other.size) return null
-        val matrixSum = IntMatrix(rows, cols)
+        val matrixSum = Matrix(rows, cols)
         elements.mapIndexed { i, row -> matrixSum[i] = sum(row, other[i]) }
         return matrixSum
     }
 
-    operator fun times(scalar: Int): IntMatrix {
-        val scalarProduct = IntMatrix(rows, cols)
+    operator fun times(scalar: Double): Matrix {
+        val scalarProduct = Matrix(rows, cols)
         elements.mapIndexed { i, row -> scalarProduct[i] = product(scalar, row) }
         return scalarProduct
     }
 
-    private fun transpose(): IntMatrix {
-        val transposed = IntMatrix(cols, rows)
+    private fun transpose(): Matrix {
+        val transposed = Matrix(cols, rows)
         repeat(rows) { row ->
             this[row].forEach { n ->
                 transposed[row] = transposed[row] + n
@@ -31,32 +31,32 @@ class IntMatrix(val rows: Int, val cols: Int) {
         return transposed
     }
 
-    operator fun set(row: Int, values: IntArray) {
+    operator fun set(row: Int, values: DoubleArray) {
         this.elements[row] = values
     }
 
-    operator fun set(row: Int, col: Int, value: Int) {
+    operator fun set(row: Int, col: Int, value: Double) {
         this.elements[row][col] = value
     }
 
-    operator fun get(row: Int): IntArray = elements[row]
+    operator fun get(row: Int): DoubleArray = elements[row]
 
-    operator fun get(row: Int, col: Int): Int = elements[row][col]
+    operator fun get(row: Int, col: Int): Double = elements[row][col]
 
-    private fun sum(a: IntArray, b: IntArray): IntArray =
-            a.mapIndexed { i, n -> n + b[i] }.toIntArray()
+    private fun sum(a: DoubleArray, b: DoubleArray): DoubleArray =
+            a.mapIndexed { i, n -> n + b[i] }.toDoubleArray()
 
-    private fun product(scalar: Int, a: IntArray): IntArray =
-            a.map { it * scalar }.toIntArray()
+    private fun product(scalar: Double, a: DoubleArray): DoubleArray =
+            a.map { it * scalar }.toDoubleArray()
 
-    private fun product(a: IntArray, b: IntArray): Int =
-            a.foldIndexed(0) { i, sum, value -> sum + value * b[i] }
+    private fun product(a: DoubleArray, b: DoubleArray): Double =
+            a.foldIndexed(0.0) { i, sum, value -> sum + value * b[i] }
 
-    operator fun times(other: IntMatrix): IntMatrix? {
+    operator fun times(other: Matrix): Matrix? {
         val (_, cols) = other.size
         if (this.rows != cols) return null
 
-        val product = IntMatrix(this.rows, cols)
+        val product = Matrix(this.rows, cols)
         val otherColumns = other.transpose()
         elements.forEachIndexed { row, rowMatrix ->
             repeat(rowMatrix.size) { col ->
@@ -67,8 +67,8 @@ class IntMatrix(val rows: Int, val cols: Int) {
     }
 }
 
-// Make it commutative: scalar * IntMatrix == IntMatrix * scalar
-operator fun Int.times(matrix: IntMatrix): IntMatrix = matrix.times(this)
+// Make it commutative: scalar * Matrix == Matrix * scalar
+operator fun Double.times(matrix: Matrix): Matrix = matrix.times(this)
 
 fun main() {
     do {
@@ -91,27 +91,30 @@ private fun chooseAction(): Int {
 }
 
 private fun sum() {
-    val (a, b) = Pair(intMatrix(), intMatrix())
+    val (a, b) = Pair(matrix(), matrix())
     println(a + b)
 }
 
 private fun scalarProduct() {
-    val a = intMatrix()
-    val scalar = readInts(1).first()
+    val a = matrix()
+    val scalar = readDoubles(1).first()
     println(scalar * a)
 }
 
 private fun matrixProduct() {
-    val (a, b) = Pair(intMatrix(), intMatrix())
+    val (a, b) = Pair(matrix(), matrix())
     println(a * b)
 }
 
-private fun intMatrix(): IntMatrix {
+private fun matrix(): Matrix {
     val (rows, cols) = readInts(2)
-    val matrix = IntMatrix(rows, cols)
-    repeat(rows) { matrix[it] = readInts(cols) }
+    val matrix = Matrix(rows, cols)
+    repeat(rows) { matrix[it] = readDoubles(cols) }
     return matrix
 }
 
 private fun readInts(count: Int): IntArray = readLine()!!.trim().split(" ")
         .map { it.toInt() }.take(count).toIntArray()
+
+private fun readDoubles(count: Int): DoubleArray = readLine()!!.trim().split(" ")
+        .map { it.toDouble() }.take(count).toDoubleArray()
