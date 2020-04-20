0c166b16123fc993224b763bfbe8ce9cc5ccc8eb
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2020-04-20 01:07:27 -0400

    Passed Stage #1 tests locally

--- Numeric_Matrix_Processor/Numeric Matrix Processor/task/src/processor/Main.kt
+++ Numeric_Matrix_Processor/Numeric Matrix Processor/task/src/processor/Main.kt
@@ -1,5 +1,34 @@
 package processor
 
 fun main() {
-    println("Hello World!")
+    val (rowsA, colsA) = readMatrixSize()
+    val matrixA = readMatrixElements(rowsA, colsA)
+
+    val (rowsB, colsB) = readMatrixSize()
+    if (rowsA != rowsB || colsA != colsB) {
+        println("ERROR")
+    } else {
+        sumOf(matrixA, readMatrixElements(rowsB, colsB)).also {
+            it.forEach { row -> println(row.joinToString(" ")) }
+        }
+    }
 }
+
+private fun readMatrixSize(): IntArray = readInts(2)
+
+private fun readMatrixElements(rows: Int, cols: Int): Array<IntArray> {
+    val matrix = Array<IntArray>(rows) { IntArray(cols) { 0 } }
+    repeat(rows) { matrix[it] = readInts(cols) }
+    return matrix
+}
+
+private fun readInts(count: Int) = readLine()!!.trim().split(" ")
+        .map { it.toInt() }.take(count).toIntArray()
+
+private fun sumOf(matrixA: Array<IntArray>, matrixB: Array<IntArray>): Array<IntArray> {
+    val result = matrixA.mapIndexed { index, row -> rowMatrixSum(row, matrixB[index]) }
+    return result.toTypedArray()
+}
+
+private fun rowMatrixSum(a: IntArray, b: IntArray): IntArray =
+    a.mapIndexed { i, n -> n + b[i] }.toIntArray()
