5aa8585f937b2d650535ca4322b73d185d6595a9
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2020-04-20 12:55:17 -0400

    Work in progress

--- Numeric_Matrix_Processor/Numeric Matrix Processor/task/src/processor/Main.kt
+++ Numeric_Matrix_Processor/Numeric Matrix Processor/task/src/processor/Main.kt
@@ -1,11 +1,12 @@
 package processor
 
-class IntMatrix (val rows: Int, val cols: Int) {
+class IntMatrix(val rows: Int, val cols: Int) {
     val size = Pair(rows, cols)
     private val elements = Array<IntArray>(rows) { IntArray(cols) { 0 } }
 
-    override fun toString(): String = elements.map { it.joinToString(" ") }
-        .joinToString("\n")
+    override fun toString(): String = elements.joinToString("\n") {
+        it.joinToString(" ")
+    }
 
     operator fun plus(other: IntMatrix): IntMatrix? {
         if (this.size != other.size) return null
@@ -15,26 +16,94 @@ class IntMatrix (val rows: Int, val cols: Int) {
     }
 
     operator fun times(scalar: Int): IntMatrix {
-        val lineArray = IntArray(cols) { scalar }
         val scalarProduct = IntMatrix(rows, cols)
-        elements.mapIndexed { i, row -> scalarProduct[i] = product(row, lineArray) }
+        elements.mapIndexed { i, row -> scalarProduct[i] = product(scalar, row) }
         return scalarProduct
     }
 
-    operator fun set(i: Int, elements: IntArray) { this.elements[i] = elements }
-    operator fun get(i: Int): IntArray = elements[i]
+    private fun transpose(): IntMatrix {
+        val transposed = IntMatrix(cols, rows)
+        repeat(rows) { row ->
+            this[row].forEach { n ->
+                transposed[row] = transposed[row] + n
+            }
+        }
+        return transposed
+    }
+
+    operator fun set(row: Int, values: IntArray) {
+        this.elements[row] = values
+    }
+
+    operator fun set(row: Int, col: Int, value: Int) {
+        this.elements[row][col] = value
+    }
+
+    operator fun get(row: Int): IntArray = elements[row]
+
+    operator fun get(row: Int, col: Int): Int = elements[row][col]
 
     private fun sum(a: IntArray, b: IntArray): IntArray =
-        a.mapIndexed { i, n -> n + b[i] }.toIntArray()
+            a.mapIndexed { i, n -> n + b[i] }.toIntArray()
 
-    private fun product(a: IntArray, b: IntArray): IntArray =
-        a.mapIndexed { i, n -> n * b[i] }.toIntArray()
+    private fun product(scalar: Int, a: IntArray): IntArray =
+            a.map { it * scalar }.toIntArray()
+
+    private fun product(a: IntArray, b: IntArray): Int =
+            a.foldIndexed(0) { i, sum, value -> sum + value * b[i] }
+
+    operator fun times(other: IntMatrix): IntMatrix? {
+        val (_, cols) = other.size
+        if (this.rows != cols) return null
+
+        val product = IntMatrix(this.rows, cols)
+        val otherColumns = other.transpose()
+        elements.forEachIndexed { row, rowMatrix ->
+            repeat(rowMatrix.size) { col ->
+                product[row][col] = product(rowMatrix, otherColumns[col])
+            }
+        }
+        return product
+    }
 }
 
+// Make it commutative: scalar * IntMatrix == IntMatrix * scalar
+operator fun Int.times(matrix: IntMatrix): IntMatrix = matrix.times(this)
+
 fun main() {
+    do {
+        val action = chooseAction()
+        when (action) {
+            1 -> sum()
+            2 -> scalarProduct()
+            3 -> matrixProduct()
+        }
+    } while (action != 0)
+}
+
+private fun chooseAction(): Int {
+    println("""|1. Add matrices
+        |2. Multiply matrix to a constant
+        |3. Multiply matrices
+        |0. Exit
+        |Your choice: """.trimMargin())
+    return readLine()!!.trim().toInt()
+}
+
+private fun sum() {
+    val (a, b) = Pair(intMatrix(), intMatrix())
+    println(a + b)
+}
+
+private fun scalarProduct() {
     val a = intMatrix()
     val scalar = readInts(1).first()
-    println(a * scalar)
+    println(scalar * a)
+}
+
+private fun matrixProduct() {
+    val (a, b) = Pair(intMatrix(), intMatrix())
+    println(a * b)
 }
 
 private fun intMatrix(): IntMatrix {
@@ -44,5 +113,5 @@ private fun intMatrix(): IntMatrix {
     return matrix
 }
 
-private fun readInts(count: Int) = readLine()!!.trim().split(" ")
+private fun readInts(count: Int): IntArray = readLine()!!.trim().split(" ")
         .map { it.toInt() }.take(count).toIntArray()
